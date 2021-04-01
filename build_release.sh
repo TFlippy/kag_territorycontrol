52f4c1bd8231b4d8baf70a2274a8d8a9c332d9c3
Vamist <44256866+Vam-Jam@users.noreply.github.com>
2021-04-02 00:34:19 +0100

    Cleaned up build_release a bit

--- build_release.sh
+++ build_release.sh
@@ -13,18 +13,12 @@ delete_thing () {
 }
 
 
-
 ## START
-
 echo "\# Clearing files"
 
 delete_thing "TerritoryControl_Autumn_Dev.zip"
 delete_thing "README.md"
 delete_thing "TFlippy_TerritoryControl_Winter_Dev" # TODO: Toggle 
-
-
-
-
 
 # TODO: Allow us to change v130
 echo "# Renaming dev to v130"
@@ -32,21 +26,14 @@ echo "# Renaming dev to v130"
 for file in *
 do
     if [[ ${file: -4} == "_Dev" ]]; then
-        echo $file
         new_name=${file::-3}
         new_name=$new_name"v130"
         mv $file $new_name
     fi
 done
 
-
-
-
-
 echo "# Moving .ogg to a new folder"
-
 mkdir $SOUND_FOLDER 2>/dev/null
-
 list="$(find . -name '*.ogg')"
 
 for file in $list
@@ -54,6 +41,5 @@ do
     mv $file $SOUND_FOLDER/ 2>/dev/null
     rm -rf $file
 done
-
 
 echo "# Done!"
